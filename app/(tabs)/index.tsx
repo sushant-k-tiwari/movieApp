1ffd9620dd30d84fd98ff3d5deb1971419b0414d
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-08-14 14:07:29 +0530

    feat: search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,35 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, StatusBar, View } from "react-native";
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center ">
-      <Text className="text-5xl text-dark-100 font-bold">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <StatusBar barStyle="light-content" />
+      <Image source={images.bg} className="absolute w-full  z-0" />
+
+      <ScrollView
+        className="flex-1"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ height: "100%", marginBottom: 10 }}
+      >
+        <View className="flex-1 px-5">
+          <Image
+            source={icons.logo}
+            className="w-12 h-10 z-10 mx-auto mt-20 mb-5"
+          />
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress: () => void;
+}
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor={"#AB8BFF"}
+      />
+      <TextInput
+        placeholder={placeholder}
+        onPress={onPress}
+        className="flex-1 ml-2 text-white"
+        onChangeText={() => {}}
+        placeholderTextColor={"#AB8BFF"}
+        keyboardType="default"
+        autoCapitalize="words"
+        autoCorrect={false}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({});
